1fbfca992cf600bdb9c0f5f91b63e0bb2b55aa58
Maya akter <mayaakter1096@gmail.com>
2026-01-18 11:03:41 +0600

    update users role to  make admin

--- src/components/dashboard/adminDashboard/users/UsersTable.jsx
+++ src/components/dashboard/adminDashboard/users/UsersTable.jsx
@@ -4,10 +4,12 @@ import { FaEdit, FaTrash } from "react-icons/fa";
 import useAxiosSecure from '../../../../hooks/useAxiosSecure';
 import { useMutation } from '@tanstack/react-query';
 import LoadingSpiner from '../../../../pages/error pages/LoadingSpiner';
+import toast from 'react-hot-toast';
+
 
 const UsersTable = ({ users, isLoading, isError, refetch }) => {
     const axiosSecure = useAxiosSecure();
-    
+
 
     const deleteMutation = useMutation({
         mutationFn: async (id) => axiosSecure.delete(`/admin/users/${id}`),
@@ -19,6 +21,22 @@ const UsersTable = ({ users, isLoading, isError, refetch }) => {
             Swal.fire("Error", error?.response?.data?.message || "Failed to delete", "error");
         },
     });
+
+    // make admin
+    const handleMakeAdmin = async (id) => {
+        try {
+            const res = await axiosSecure.patch(`/users/make-admin/${id}`);
+
+            if (res.data.success) {
+                toast.success("User promoted to admin");
+                refetch();
+            }
+        } catch (error) {
+            toast.error("Failed to make admin", error);
+        }
+    };
+
+
 
     const handleDelete = (id) => {
         Swal.fire({
@@ -64,9 +82,9 @@ const UsersTable = ({ users, isLoading, isError, refetch }) => {
                             <th className="py-3 px-4 border-b">#</th>
                             <th className="py-3 px-4 border-b">Image</th>
                             <th className="py-3 px-4 border-b">Name</th>
-                        <th className="py-3 px-4 border-b">Email</th>
+                            <th className="py-3 px-4 border-b">Email</th>
                             <th className="py-3 px-4 border-b">Role</th>
-                            
+
                             <th className="py-3 px-4 border-b text-center">Actions</th>
                         </tr>
                     </thead>
@@ -83,18 +101,30 @@ const UsersTable = ({ users, isLoading, isError, refetch }) => {
                                 </td>
                                 <td className="py-3 px-4 border-gray-200 border-b font-medium">{user.name}</td>
                                 <td className="py-3 px-4 border-gray-200 border-b">{user.email}</td>
-                               
-                                
-                                
+
+
+
                                 <td className="py-3 px-4 border-gray-200 border-b">{user.role}</td>
-                                
+
                                 <td className="py-3 px-4 border-gray-200 border-b text-center space-x-2">
+                                    <button
+                                        onClick={() => handleMakeAdmin(user._id)}
+                                        disabled={user.role === "admin"}
+                                        className={`px-3 py-1 rounded text-md font-bold text-white
+    ${user.role === "admin"
+                                                ? "bg-gray-400 cursor-not-allowed"
+                                                : "bg-green-500 hover:bg-green-600"}
+  `}
+                                    >
+                                        {user.role === "admin" ? "Admin" : "Make Admin"}
+                                    </button>
 
                                     <button
                                         onClick={() => handleDelete(user._id)}
                                         className="text-red-500 text-xl px-3 py-1 rounded hover:text-red-600"
+                                        title='delete user'
                                     >
-                                        <FaTrash/>
+                                        <FaTrash />
                                     </button>
                                 </td>
                             </tr>
