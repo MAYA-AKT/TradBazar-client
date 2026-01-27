9ed877f31e376fb41c09b8016a1a5798954cab79
Maya akter <mayaakter1096@gmail.com>
2026-01-27 21:31:07 +0600

    change base url

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -8,9 +8,9 @@ import useAuth from './useAuth';
 
 
 const axiosSecure = axios.create({
-   baseURL: "https://tradbazar-server.vercel.app"
-//    baseURL:"http://103.191.240.201:3000"
-    //   baseURL:"http://localhost:3000"
+//    baseURL: "https://tradbazar-server.vercel.app"
+  baseURL:"http://103.191.240.201:3000"
+    // baseURL:"http://localhost:3000"
 });
 
 
